2ec3de7578b551fb9a65c639ab039f5292f0a312
theSuriya <thesuriya3@gmail.com>
2023-12-04 18:30:28 +0530

    added

---
+++ frontend/static/script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("uploadForm");
+    const fileInput = document.getElementById("fileInput");
+    const imagePreview = document.getElementById("imagePreview");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        fetch("/predict", {
+            method: "POST",
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            displayPredictionResult(data);
+        })
+        .catch(error => console.error("Error:", error));
+    });
+
+    // Image preview on file input change
+    fileInput.addEventListener("change", function () {
+        const file = fileInput.files[0];
+        if (file) {
+            displayImagePreview(file);
+        }
+    });
+});
+
+function displayPredictionResult(data) {
+    const resultDiv = document.getElementById("predictionResult");
+    resultDiv.innerHTML = `
+        <p>Predicted Pose: ${data.class}</p>
+        <p>Confidence: ${data.confidence}%</p>
+    `;
+}
+
+function displayImagePreview(file) {
+    const imagePreview = document.getElementById("imagePreview");
+    const imagePreviewContainer = document.getElementById("imagePreviewContainer");
+
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        imagePreview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+    imagePreviewContainer.style.display = "block";
+}
---
+++ main.py
@@ -0,0 +1,105 @@
+import uvicorn
+from fastapi import FastAPI,File,UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
+from PIL import Image
+from io import BytesIO
+from keras.models import load_model
+from fastapi import Request
+import os
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+api_directory = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the model relative to the 'api' directory
+model_path = os.path.join(api_directory, 'model', 'mammals V1 model.keras')
+
+model = load_model(model_path)
+
+class_name = ['african_elephant','alpaca','american_bison','anteater','arctic_fox','armadillo','baboon','badger','blue_whale','brown_bear','camel','dolphin','giraffe','groundhog',
+'highland_cattle',
+'horse',
+'jackal',
+'kangaroo',
+'koala',
+ 'manatee',
+ 'mongoose',
+ 'mountain_goat',
+ 'opossum',
+ 'orangutan',
+ 'otter',
+ 'polar_bear',
+ 'porcupine',
+ 'red_panda',
+ 'rhinoceros',
+ 'sea_lion',
+ 'seal',
+ 'snow_leopard',
+ 'squirrel',
+ 'sugar_glider',
+ 'tapir',
+ 'vampire_bat',
+ 'vicuna',
+ 'walrus',
+ 'warthog',
+ 'water_buffalo',
+ 'weasel',
+ 'wildebeest',
+ 'wombat',
+ 'yak',
+ 'zebra']
+
+
+
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+templates = Jinja2Templates(directory="frontend")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get('/ping')
+async def check():
+    return "Hello world"
+
+
+def read_file_as_image(data):
+    img = Image.open(BytesIO(data)).resize((224, 224))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    return img_array
+
+
+@app.post('/predict')
+async def prediction(file: UploadFile = File(...)):
+
+     img = read_file_as_image(await file.read())
+     img = np.expand_dims(img, axis=0)
+
+     predicted =  model.predict(img)
+     result = class_name[np.argmax(predicted[0])]
+     confidence = np.max(predicted[0])
+
+     return{
+         'class': result,
+         'confidence':round(confidence * 100, 1)
+     }
+
+if __name__ == '__main__':
+    uvicorn.run(app,host='localhost',port=8000)
